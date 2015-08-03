463cec239478f7eadee4ee7588840026de50ab9e
nsieber <nsieber@gmail.com>
2015-08-03 11:52:12 -0400

    new script file and change list margins on smaller screens

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
-$(document).ready(function() { 
-	$('button').on('click', function() {
-		var item = $("input[type='text']").val();
-		$('p:last-child').remove();
-		$('.list').append('<p><input type="checkbox">&nbsp;' + item + '<button type="button">remove</button></p>');
-		$('.list').append('<p><input type="text" name="item"><button name="item">add</button></p>');
-	});
-	$('input[name="item"]').keypress(function(e){
-        if(e.which == 13){//Enter key pressed
-            $('button[name="item"]').click();//Trigger search button click event
-        }
-    });
-});
+(function($) {
+    "use strict"
+
+    var itemList
+
+    $(document).ready(function() {
+        itemList = $(".list p")
+    })
+
+}(jQuery))
