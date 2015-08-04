755913983063625791f2d355dbd60bd0f1f722b9
nsieber <nsieber@gmail.com>
2015-08-04 19:50:42 -0400

    working on new script file to implement functionality with better designed code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
 (function($) {
     "use strict"
 
-    var itemList
-
+    var itemList, listLength
+    var itemButton = $('button[name="item"]')
+    
     $(document).ready(function() {
         itemList = $(".list p")
+        listLength = itemList.length
+        
+        itemButton.on('click', function() {
+            console.log('ok')
+        })
+        itemButton.on('click', function() {
+            console.log('I\'m clicked')
+        })
     })
+
+    function addItem() {
+       // $(".list").append(getItem())
+        console.log(getItem())
+    }
+    
+    function getItem() {
+        $('button[name="item"]').on('click', function() {
+            return $("input[type='text']").val()
+       })
+    }
+
+    function printList(arr) {
+        var len = arr.length
+        for(var i = 0; i < len; i++) {
+            console.log(arr[i]);
+        }   
+    }
 
 }(jQuery))
