f63a65021086ba1febd66eb8fd1a8779f3a15d75
nsieber <nsieber@gmail.com>
2015-08-06 22:17:39 -0400

    ready to send for code review

--- js/script.current.js
+++ js/script.current.js
@@ -1,16 +1,18 @@
 (function($) {
     "use strict"
 
-//    var itemList, listLength
+    var itemList, listLength
     var prepend = '<p><input type="checkbox">&nbsp;'
     var append = '<button ID="remove">remove</button></p>'
     $(document).ready(function() {
-//        itemList = $(".list p")
-//        listLength = itemList.length
+        itemList = $(".list p")
+        listLength = itemList.length
         
         $('#item').on('click', function() {
-            addItem($('#input'))
+//            addItem($('#input'))
+              $('.list').append('#input')
         })    
+
     })
 
     function addItem(item) {
