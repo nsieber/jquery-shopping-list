ace304def41d5bef2f77420d67bcd8c16c4b95b0
nsieber <nsieber@gmail.com>
2015-08-06 20:37:04 -0400

    restructure script.js and use I Ds to access DOM

--- js/script.js
+++ js/script.js
@@ -2,36 +2,17 @@
     "use strict"
 
     var itemList, listLength
-    var itemButton = $('button[name="item"]')
     
     $(document).ready(function() {
         itemList = $(".list p")
         listLength = itemList.length
         
-        itemButton.on('click', function() {
-            console.log('ok')
-        })
-        itemButton.on('click', function() {
-            console.log('I\'m clicked')
-        })
+        $('#item').on('click', function() {
+            console.log($('#input').val())
+        })    
+
+
+
     })
-
-    function addItem() {
-       // $(".list").append(getItem())
-        console.log(getItem())
-    }
-    
-    function getItem() {
-        $('button[name="item"]').on('click', function() {
-            return $("input[type='text']").val()
-       })
-    }
-
-    function printList(arr) {
-        var len = arr.length
-        for(var i = 0; i < len; i++) {
-            console.log(arr[i]);
-        }   
-    }
 
 }(jQuery))
